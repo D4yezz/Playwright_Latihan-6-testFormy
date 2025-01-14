0548b118fdaee1d2523b2a4f550d8d1d12336fd4
Dayezzz <adiasmuhsin1206@gmail.com>
2025-01-14 15:57:28 +0700

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,28 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Form Submit', async ({ page }) => {
+  await page.goto('https://formy-project.herokuapp.com/');
+await page.getByRole('link', { name: 'Form', exact: true }).click();
+await expect(page).toHaveURL('https://formy-project.herokuapp.com/form');
+
+await page.waitForTimeout(2000);
+
+await page.getByPlaceholder('Enter first name').fill('Diasss');
+await page.getByPlaceholder('Enter last name').fill('Dayezz');
+await page.getByPlaceholder('Enter your job title').fill('Softawre Engineer');
+await page.locator('#radio-button-1').click();
+await page.locator('#checkbox-1').click();
+await page.locator('select#select-menu').selectOption({ index: 2 });
+await page.getByPlaceholder('mm/dd/yyyy').fill('12/06/2024');
+
+await page.getByRole('button', { name: 'Submit' }).click();
+
+await expect(page.locator('.alert.alert-success')).toBeVisible();
+
+});
+
+test('drag and drop', async ({ page }) => {
+  await page.goto('https://formy-project.herokuapp.com/dragdrop');
+await page.locator('#image').dragTo(await page.locator('#box'));
+});
